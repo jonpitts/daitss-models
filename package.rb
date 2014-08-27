b3ddd7f18d285c956da23c9290027efbf451056a
Jon <jpitts@flvc.org>
2014-08-27 16:11:50 -0400

    remove proc from package

--- package.rb
+++ package.rb
@@ -15,7 +15,7 @@ class Package < Sequel::Model(:packages)
   #set default values
   def before_create
     super
-    self.uri ||= proc { |r,p| "info:fda/" + r.id } #will have issues
+    self.uri ||= "info:fda/" + self.id 
   end
   #save defaults to raise on save failure. false returns nil
   self.raise_on_save_failure = false 
